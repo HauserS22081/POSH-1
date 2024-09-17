617da14f56444c187dcee6051158f2a24946cffb
HauserS22081 <HauserS22081@sus.htl-grieskirchen.at>
2024-09-17 11:14:28 +0200

    Initial Projektinitialization

---
+++ Primechecker/src/net/htlgkr/hausers22081/pos3/hue1/EratosthenesPrimeSieve.java
@@ -0,0 +1,4 @@
+package net.htlgkr.hausers22081.pos3.hue1;
+
+public class EratosthenesPrimeSieve {
+}
---
+++ Primechecker/src/net/htlgkr/hausers22081/pos3/hue1/PrimeSieve.java
@@ -0,0 +1,7 @@
+package net.htlgkr.hausers22081.pos3.hue1;
+
+public interface PrimeSieve {
+    public boolean isPrime(int p);
+
+    public void printPrimes();
+}
